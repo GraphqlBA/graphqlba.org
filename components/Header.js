ff395eb75fd440686a0a0bcb39127de5cabd918f
Pablo Chiappetti <pchiappetti@gmail.com>
2017-04-09 02:52:30 -0300

    commit inicial

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+export default () => (
+  <header>
+    <style jsx>{`
+      header {
+        background: #202020;
+        height: 150px;
+        display: flex;
+        justify-content: center;
+      }
+      img {
+        height: 100%;
+      }
+    `}</style>
+    <img src="/static/logo.png" alt="" />
+  </header>
+)
---
+++ styles/main.js
@@ -0,0 +1,12 @@
+export default () => (
+  <div>
+    <style jsx global>{`
+      html, body {
+        padding: 0;
+        border: 0;
+        margin: 0;
+        background: #202020;
+      }
+    `}</style>
+  </div>
+)
